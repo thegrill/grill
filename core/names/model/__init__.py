1cff5a57195bb6337e1d5d3a6c03cae016cfcb99
Christian López Barrón <christianlb.vfx@outlook.com>
2016-09-11 12:42:49 +0200

    minimized core.names import

---
+++ core/names/model/__init__.py
@@ -0,0 +1,7 @@
+from .audiovisual import Audiovisual
+from .base import Name
+from .environment import Environment
+from .file import File
+from .film import Film
+from .pipe import Pipe
+from .project import Project
