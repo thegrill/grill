d04b9e16486651e9832ec1501d7ef2079057bf7a
Christian López Barrón <christianlb.vfx@outlook.com>
2016-09-14 23:53:32 +0200

    fix for invalid logger names

--- utils.py
+++ utils.py
@@ -14,6 +14,6 @@ def toUnderscores(string):
 
 def toCamelCase(string):
     parts = re.findall('([a-zA-Z0-9]+)', string)
-    for i, p in enumerate(parts):
+    for i, p in enumerate(parts[1:], start=1):
         parts[i] = '{}{}'.format(p[0].upper(), p[1:])
     return ''.join(parts)
