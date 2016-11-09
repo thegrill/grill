3b4561884f38aaa36cffa6ab720d2f7e5c0a3abc
Christian López Barrón <christianlb.vfx@outlook.com>
2016-11-09 19:06:53 +0100

    added testPipe to core.names test, removed unused buildtest files

--- core/names/test.py
+++ core/names/test.py
@@ -10,11 +10,28 @@ from . import model
 
 class TestNames(unittest.TestCase):
 
-    def testBase(self):
+    def testBaseName(self):
         name = model.base.Name()
         name.getName()
         self.assertFalse(name.getValues())
         name_str = 'basename'
         name.setName(name_str)
         values = name.getValues()
-        assert values['base'] == name_str == name.nice_name == name.name
+        self.assertTrue(values['base'] == name_str == name.nice_name == name.name)
+
+
+    def testPipe(self):
+        name = model.pipe.Pipe()
+        name.getName()
+        self.assertFalse(name.getValues())
+        name_str = 'basename_data'
+        name.setName(name_str)
+        values = name.getValues()
+        self.assertEqual(values['base'], name.nice_name)
+        self.assertEqual(values['output'], 'data')
+        self.assertEqual(name_str, name.name)
+        self.assertEqual(name.getName(pipe='_geo'), 'basename_geo')
+        self.assertEqual(name.getName(version=17), 'basename.17')
+        name.setName(name.getName(output='cache', frame=101))
+        self.assertEqual(name.output, 'cache')
+        self.assertEqual(name.frame, '101')
