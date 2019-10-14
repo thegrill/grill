f06796e48d0a753ff2236c1ab444f4ea1ab69e97
Christian López Barrón <chris.gfz@gmail.com>
2019-10-15 01:31:47 +1100

    updated logger package

--- grill/logger/model.py
+++ grill/logger/model.py
@@ -3,7 +3,15 @@
 Grill logging module.
 """
 # standard
+from __future__ import annotations
+
 import logging
+from pathlib import Path
+from datetime import datetime
+
+from naming import File, NameConfig
+
+_LOG_FILE_SUFFIX = 'log'
 
 
 class ErrorFilter(logging.Filter):
@@ -14,7 +22,7 @@ class ErrorFilter(logging.Filter):
         """
         If the record does is not logging.INFO, return True
         """
-        return record.levelno != logging.INFO
+        return record.levelno > logging.INFO
 
 
 class OutFilter(logging.Filter):
@@ -25,4 +33,38 @@ class OutFilter(logging.Filter):
         """
         If the record does is logging.INFO, return True
         """
-        return record.levelno == logging.INFO
+        return record.levelno <= logging.INFO
+
+
+class LogFile(File):
+    """docstring for LogFile"""
+    config = dict(
+        date=r'\d{4}-(0[1-9]|1[0-2])-([0-2][1-9]|3[01])',
+        clock=r'([01]?[0-9]|2[0-4])-[0-5]?[0-9]-[0-5]?[0-9]',
+        micosecond=r'\d{1,6}',
+        log_name=r'[\w\.]+',
+        log_filter='\d+',
+    )
+    file_config = NameConfig(dict(suffix=_LOG_FILE_SUFFIX))
+
+    @property
+    def path(self):
+        return Path(r'~/grill').expanduser() / super().name
+
+    @classmethod
+    def get_default(cls, **kwargs) -> LogFile:
+        """Get a new Name object with default values and overrides from **kwargs."""
+        now = datetime.now()
+        name = cls()
+        defaults = dict(
+            date=f'{now.year}-{now.month}-{now.day}',
+            clock=f'{now.hour}-{now.minute}-{now.second}',
+            micosecond=now.microsecond,
+            log_name='grill',
+            log_filter=logging.INFO,
+            suffix=_LOG_FILE_SUFFIX,
+        )
+        defaults.update(kwargs)
+        name.name = name.get_name(**defaults)
+        return name
+
