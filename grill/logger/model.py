c900dfc385609734c74d57879bb9ff36780d06ec
Christian López Barrón <chris.gfz@gmail.com>
2019-10-26 14:58:38 +1100

    added timefile

--- grill/logger/model.py
+++ grill/logger/model.py
@@ -5,6 +5,7 @@ Grill logging module.
 # standard
 from __future__ import annotations
 
+import typing
 import logging
 from pathlib import Path
 from datetime import datetime
@@ -36,15 +37,70 @@ class OutFilter(logging.Filter):
         return record.levelno <= logging.INFO
 
 
-class LogFile(File):
+class TimeFile(File):
+    """Time based file names respecting iso standard
+    """
+    config = dict.fromkeys(
+        ('year', 'month', 'day', 'hour', 'minute', 'second', 'microsecond'), r'\d{1,2}'
+    )
+    config.update(year=r'\d{1,4}', microsecond=r'\d{1,6}')
+    join = dict(
+        date=('year', 'month', 'day'),
+        time=('hour', 'minute', 'second', 'microsecond'),
+    )
+    join_sep = '-'
+
+    def get_pattern_list(self) -> typing.List[str]:
+        return ["date", "time"]
+
+    @property
+    def name(self):
+        return super().name
+
+    @name.setter
+    def name(self, name: str):
+        prev_name = self._name
+        super(TimeFile, self.__class__).name.fset(self, name)
+        # validate with datetime isoformat directly
+        if name:
+            isodate = f"{int(self.year):04d}-{int(self.month):02d}-{int(self.day):02d}"
+            isoclock = f"{int(self.hour):02d}:{int(self.minute):02d}:{int(self.second):02d}.{int(self.microsecond):06d}"
+            # if iso validation fails, we fail
+            # if we had a previous valid name, revert to it
+            try:
+                datetime.fromisoformat(f'{isodate}T{isoclock}')
+            except ValueError:
+                if prev_name:
+                    self.name = prev_name
+                raise
+
+    @classmethod
+    def get_default(cls, **kwargs) -> TimeFile:
+        """ Get a new Name object with default values and overrides from **kwargs.
+
+        Note: `join` fields can not be overridden by **kwargs as the atomic fields
+              take precedence. To override compounds, pass each individual field:
+
+              >>>
+        """
+        now = datetime.now()
+        name = cls()
+        time_field = {'year', 'month', 'day', 'hour', 'minute', 'second', 'microsecond'}
+        defaults = dict(
+            {f: getattr(now, f) for f in time_field},
+            suffix='ext',
+        )
+        defaults.update(kwargs)
+        name.name = name.get_name(**defaults)
+        return name
+
+class LogFile(TimeFile):
     """docstring for LogFile"""
     config = dict(
-        date=r'\d{4}-(0?[1-9]|1[0-2])-([0-2]?[1-9]|3[01])',
-        clock=r'([01]?[0-9]|2[0-4])-[0-5]?[0-9]-[0-5]?[0-9]',
-        microsecond=r'\d{1,6}',
         log_name=r'[\w\.]+',
         log_filter=r'\d+',
     )
+    join_sep = '-'
     file_config = NameConfig(dict(suffix=_LOG_FILE_SUFFIX))
 
     @property
@@ -53,13 +109,18 @@ class LogFile(File):
 
     @classmethod
     def get_default(cls, **kwargs) -> LogFile:
-        """Get a new Name object with default values and overrides from **kwargs."""
+        """ Get a new Name object with default values and overrides from **kwargs.
+
+        Note: `join` fields can not be overridden by **kwargs as the atomic fields
+              take precedence. To override compounds, pass each individual field:
+
+              >>>
+        """
         now = datetime.now()
         name = cls()
+        time_field = {'year', 'month', 'day', 'hour', 'minute', 'second', 'microsecond'}
         defaults = dict(
-            date=f'{now.year}-{now.month}-{now.day}',
-            clock=f'{now.hour}-{now.minute}-{now.second}',
-            microsecond=now.microsecond,
+            {f: getattr(now, f) for f in time_field},
             log_name='grill',
             log_filter=logging.INFO,
             suffix=_LOG_FILE_SUFFIX,
@@ -67,4 +128,3 @@ class LogFile(File):
         defaults.update(kwargs)
         name.name = name.get_name(**defaults)
         return name
-
--- tests/test_logger.py
+++ tests/test_logger.py
@@ -1,5 +1,5 @@
 import unittest
-
+from grill.logger import model
 
 class TestLogger(unittest.TestCase):
 
@@ -11,3 +11,41 @@ class TestLogger(unittest.TestCase):
         from grill.logger import get_logger
         logger = get_logger(__name__)
         logger.info("Testing inner module")
+
+    def test_default(self):
+        l = model.LogFile.get_default(year=1999)
+        self.assertEqual(l.year, '1999')
+
+        with self.assertRaises(ValueError):
+            # invalid year 0
+            l.month = 0
+
+        with self.assertRaises(ValueError):
+            # max year 9999
+            l.month = 12345
+
+        for y in range(1, 1000):
+            l.year = y
+            self.assertEqual(l.year, str(y))
+
+        with self.assertRaises(ValueError):
+            # invalid month 13
+            l.month = 13
+
+        for m in range(1,13):
+            l.month = m
+
+        with self.assertRaises(ValueError):
+            # invalid month 0
+            l.month = 0
+
+        with self.assertRaises(ValueError):
+            # invalid month 13
+            l.day = 0
+
+        for m in range(1,32):
+            l.day = m
+
+        with self.assertRaises(ValueError):
+            # invalid day 32
+            l.day = 32
