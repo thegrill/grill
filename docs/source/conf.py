21f18a7c27fd81a90edc76fc599de2d5880ebf38
Christian López Barrón <chris.gfz@gmail.com>
2020-11-17 23:23:20 +1100

    try sphinx_typlog_theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -101,7 +101,8 @@ todo_include_todos = True
 #
 # html_theme = 'alabaster'
 # html_theme = 'nature'
-html_theme = 'python_docs_theme'
+# html_theme = 'python_docs_theme'
+html_theme = 'sphinx_typlog_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
