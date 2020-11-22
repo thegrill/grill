7e6b481b3fe8177e4f1154fad9fe999eb52a5c6a
Christian López Barrón <chris.gfz@gmail.com>
2020-11-22 13:02:17 +1100

    separator under badges

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -99,9 +99,7 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-# html_theme = 'alabaster'
-# html_theme = 'nature'
-# html_theme = 'python_docs_theme'
+# also tried: python_docs_theme, sphinx_rtd_theme
 html_theme = 'sphinx_typlog_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
