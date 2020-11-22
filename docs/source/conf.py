f75aa573863bf1e79ed3cd642fa679ee6c2d7c00
chrizzftd <chris.gfz@gmail.com>
2020-11-22 13:12:06 +1100

    Feature/docs theme (#8)
    * use sphinx_typlog_theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -99,8 +99,8 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-# html_theme = 'alabaster'
-html_theme = 'sphinx_rtd_theme'
+# also tried: python_docs_theme, sphinx_rtd_theme
+html_theme = 'sphinx_typlog_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -191,8 +191,6 @@ epub_copyright = copyright
 
 # A list of files that should not be packed into the epub file.
 epub_exclude_files = ['search.html']
-
-
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'https://docs.python.org/': None,
