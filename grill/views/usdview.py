1c631ec4b40efb4894482fae53dff2ba8111b417
Christian López Barrón <chris.gfz@gmail.com>
2020-11-29 12:59:36 +1100

    added LayersComposition on usdview

--- grill/views/usdview.py
+++ grill/views/usdview.py
@@ -20,6 +20,14 @@ def __getattr__(name):
         editor = _spreadsheet.Spreadsheet(parent=usdviewApi.qMainWindow)
         editor.setStage(usdviewApi.stage)
         return editor
+    elif name == _USDVIEW_LAYER_STACK_COMPOSITION_KEY:
+        print("Initialising layer stack composition!")
+        usdviewApi = _USDVIEW_API.get()
+        import importlib
+        importlib.reload(_description)
+        editor = _description.LayersComposition(parent=usdviewApi.qMainWindow)
+        editor.setStage(usdviewApi.stage)
+        return editor
     elif name == _USDVIEW_PRIM_DESCRIPTION_KEY:
         print("Initialising prim description!")
         usdviewApi = _USDVIEW_API.get()
@@ -65,6 +73,21 @@ def prim_composition(usdviewApi):
     editor.show()
 
 
+_USDVIEW_LAYER_STACK_COMPOSITION_KEY = "_usdview_layer_stack_composition"
+def layer_stack_composition(usdviewApi):
+    print("Launching Layer Stack Composition!")
+    ctx = contextvars.copy_context()
+
+    def getEditor():
+        _USDVIEW_API.set(usdviewApi)
+        return __getattr__(_USDVIEW_LAYER_STACK_COMPOSITION_KEY)
+
+    editor = ctx.run(getEditor)
+    # if usdviewApi.prim:
+    #     editor.setPrim(usdviewApi.prim)
+    editor.show()
+
+
 class GrillPlugin(PluginContainer):
 
     def registerPlugins(self, plugRegistry, usdviewApi):
@@ -78,10 +101,16 @@ class GrillPlugin(PluginContainer):
             "Prim Composition",
             prim_composition)
 
+        self._layer_stack_composition = plugRegistry.registerCommandPlugin(
+            "Grill.layer_stack_composition",
+            "Layer Stack Composition",
+            layer_stack_composition)
+
     def configureView(self, plugRegistry, plugUIBuilder):
         grill_menu = plugUIBuilder.findOrCreateMenu("Grill")
         grill_menu.addItem(self._spreadsheet)
         grill_menu.addItem(self._prim_composition)
+        grill_menu.addItem(self._layer_stack_composition)
 
 
 Tf.Type.Define(GrillPlugin)
