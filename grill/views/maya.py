4c0c9b9d264a4cebbad794468f9cc13a9eee6a17
Christian López Barrón <chris.gfz@gmail.com>
2021-04-22 00:40:18 +1000

    maya style

--- grill/views/maya.py
+++ grill/views/maya.py
@@ -21,6 +21,41 @@ def maya_main_window():
 def _stage_on_widget(widget_creator):
     # TODO: allow for stage selection. For now, use the first found
     widget = widget_creator(parent=maya_main_window())
+    widget.setStyleSheet(
+        # Maya checked buttons style look ugly (all black), so we're adding a very
+        # slightly modified USDView stylesheet for the push buttons.
+        """
+        QPushButton{
+            /* gradient background */
+            background-color: QLinearGradient(x1: 0, y1: 0, x2: 0, y2: 1, stop: 0 rgb(100, 100, 100), stop: 1 rgb(90, 90, 90));
+    
+            /* thin dark round border */
+            border-width: 1px;
+            border-color: rgb(42, 42, 42);
+            border-style: solid;
+            border-radius: 3;
+    
+            /* give the text enough space */
+            padding: 3px;
+            padding-right: 10px;
+            padding-left: 10px;
+        }
+    
+        /* Darker gradient when the button is pressed down */
+        QPushButton:pressed {
+            background-color: QLinearGradient(x1: 0, y1: 0, x2: 0, y2: 1, stop: 0 rgb(50, 50, 50), stop: 1 rgb(60, 60, 60));
+        }
+    
+        QPushButton:checked {
+            background-color: QLinearGradient(x1: 0, y1: 0, x2: 0, y2: 1, stop: 0 rgb(60, 65, 70), stop: 1 rgb(70, 75, 80));
+        }
+    
+        /* Greyed-out colors when the button is disabled */
+        QPushButton:disabled {
+            background-color: QLinearGradient(x1: 0, y1: 0, x2: 0, y2: 1, stop: 0 rgb(66, 66, 66), stop: 1 rgb(56, 56, 56));
+        }
+        """
+    )
     usd_proxies = cmds.ls(typ='mayaUsdProxyShape', l=True)
     stage = next((mayaUsd.ufe.getStage(node) for node in usd_proxies), None)
     if stage:
