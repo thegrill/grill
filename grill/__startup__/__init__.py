bfebd505b1e8c8708964d55e49291398fa53c914
Christian López Barrón <chris.gfz@gmail.com>
2021-06-01 22:54:50 +1000

    fix usd resource loading

--- grill/__startup__/__init__.py
+++ grill/__startup__/__init__.py
@@ -4,14 +4,20 @@ from pathlib import Path
 
 
 def install(sitedir):
-    if "MAYA_PLUG_IN_PATH" in os.environ:
-        from . import maya
-        maya.install()
     # NOTE: if plugInfo provides more plugins other than USDView, it will need to be
     # executed in deferred evaluation in maya and extended on the houdini package.
     _usd_pluginfo(sitedir)
 
+    if "MAYA_PLUG_IN_PATH" in os.environ:  # Quick check for maya plugins
+        try:
+            from maya import cmds
+        except ImportError:  # Not a true Maya environment
+            pass
+        else:
+            from . import maya
+            maya.install()
+
 
 def _usd_pluginfo(sitedir):
-    os.environ["PXR_PLUGINPATH_NAME"] = f"{Path(sitedir) / 'grill' / 'resources' / 'plugInfo.json'}{os.pathsep}{os.environ.get('PXR_PLUGINPATH_NAME', '')}"
+    os.environ["PXR_PLUGINPATH_NAME"] = f"{Path(sitedir) / 'grill' / 'resources' / 'usd' / 'plugInfo.json'}{os.pathsep}{os.environ.get('PXR_PLUGINPATH_NAME', '')}"
 
