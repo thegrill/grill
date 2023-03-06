a4db9a5dc5536806bb7446a0316e730f42b802ea
Christian López Barrón <chris.gfz@gmail.com>
2023-03-06 21:50:04 +1100

    protect QtCharts import

--- grill/views/_qt.py
+++ grill/views/_qt.py
@@ -2,4 +2,8 @@ try:  # only while transition from PySide2 to PySide6 happens
     from PySide6 import QtWidgets, QtGui, QtCore, QtWebEngineWidgets, QtCharts
 except ImportError:
     from PySide2 import QtWidgets, QtGui, QtCore, QtWebEngineWidgets
-    from PySide2.QtCharts import QtCharts
+    try:
+        from PySide2.QtCharts import QtCharts
+    except ImportError:
+        # Maya-2023.3 and Houdini-19.5.534 bundle PySide2, but their versions fail to bring QtCharts :c
+        pass
