cb3223bec036b7534f283959ccb66ab07edf9ab7
Christian López Barrón <chris.gfz@gmail.com>
2024-06-22 17:46:15 +1000

    note about broken color editor

--- grill/views/_attributes.py
+++ grill/views/_attributes.py
@@ -71,6 +71,7 @@ def _color_spectrum(start: QtGui.QColor, end: QtGui.QColor, amount):
 class _DisplayColorEditor(QtWidgets.QFrame):
     # TODO: support alpha update
     # TODO: still experimental
+    # TODO: nicer color controls, broken in Qt6.7
     def __init__(self, primvar: UsdGeom.Primvar, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self._primvar = primvar
