2ddeaa0a847c451df4003fbaadb6c50a0ef2b4f7
Christian López Barrón <chris.gfz@gmail.com>
2024-10-26 20:43:18 +1100

    maya still does not bring qtcharts
    Signed-off-by: Christian López Barrón <chris.gfz@gmail.com>

--- grill/views/_qt.py
+++ grill/views/_qt.py
@@ -1,5 +1,10 @@
 try:  # only while transition from PySide2 to PySide6 happens
-    from PySide6 import QtWidgets, QtGui, QtCore, QtCharts, QtSvg, QtTest
+    from PySide6 import QtWidgets, QtGui, QtCore, QtSvg, QtTest
+    try:
+        from PySide6 import QtCharts
+    except ImportError:
+        # Maya-2025.3 bundles PySide6, but fails to bring QtCharts :c
+        pass
 except ImportError:
     from PySide2 import QtWidgets, QtGui, QtCore, QtSvg, QtTest
     if not hasattr(QtCore, "__enter__"):
